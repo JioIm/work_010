c44ac3d52c12c3cf7918473007c517fd5a3c04a6
Jio Im <148739315+JioIm@users.noreply.github.com>
2024-01-04 00:32:23 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,10 @@
 $(function () {
     const MAIN_CONTENT_SLIDE = new Swiper('.main_content_slide', {
-        effect: 'fade',
+        // effect: 'fade',
         loop: 'true',
         pagination: {
             el: ".swiper-pagination",
-        },
+          },
     })
 })
 
