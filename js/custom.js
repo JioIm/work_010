b0b3451e20b923b1f65f52f1a9320de6885b68a5
Jio Im <148739315+JioIm@users.noreply.github.com>
2024-01-04 18:17:28 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,30 @@
 $(function () {
+    $('.main').fullpage({
+        anchors: ["p1", "p2", "p3", "p4"],
+        afterLoad: function (anchorLink, index) {
+            console.log(anchorLink, index);
+            if (index == 2 || index == 3) {
+                $('.Header').addClass('on')
+            } else {
+                $('.Header').removeClass('on')
+            }
+        },
+        responsiveWidth: 768,
+
+    });
+
     const MAIN_CONTENT_SLIDE = new Swiper('.main_content_slide', {
         // effect: 'fade',
         loop: 'true',
         pagination: {
             el: ".swiper-pagination",
-          },
+        },
     })
+    $('.MainInfo .itm').on('mouseenter', function () {
+        $('.MainInfo .itm').removeClass('on');
+        $(this).addClass('on');
+    })
+
+
 })
 
